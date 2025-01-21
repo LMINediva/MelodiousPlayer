8434eba297a02950f4c3d5dd756c1cb3bb16513f
LMINediva <3037907347@qq.com>
2025-01-21 22:18:43 +0800

    Fixed some bugs.

--- app/src/main/java/com/melodiousplayer/android/ui/activity/JiaoZiVideoPlayerActivity.kt
+++ app/src/main/java/com/melodiousplayer/android/ui/activity/JiaoZiVideoPlayerActivity.kt
@@ -1,6 +1,7 @@
 package com.melodiousplayer.android.ui.activity
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
@@ -15,6 +16,11 @@ class JiaoZiVideoPlayerActivity : BaseActivity() {
 
     private lateinit var videoplayer: JzvdStd
     private var data: String? = null
+    private val permissions: Array<String> = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
+    private var hasPermissions: Boolean = true
 
     override fun getLayoutId(): Int {
         return R.layout.activity_video_player_jiaozi
@@ -30,20 +36,28 @@ class JiaoZiVideoPlayerActivity : BaseActivity() {
             videoplayer.setUp(videoPlayBean?.url, videoPlayBean?.title, JzvdStd.SCREEN_NORMAL)
         } else {
             // 动态申请权限
-            if (ContextCompat.checkSelfPermission(
-                    this,
-                    Manifest.permission.READ_EXTERNAL_STORAGE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this,
-                    arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), 1
-                )
+            hasPermissions = hasPermissions(this, permissions)
+            if (!hasPermissions) {
+                ActivityCompat.requestPermissions(this, permissions, 1)
             } else {
                 // 应用外响应
                 videoplayer.setUp(data.toString(), data.toString(), JzvdStd.SCREEN_NORMAL)
             }
         }
+    }
+
+    /**
+     * 检查多个权限是否授权
+     */
+    fun hasPermissions(context: Context, permissions: Array<String>): Boolean {
+        for (permission in permissions) {
+            if (ContextCompat.checkSelfPermission(context, permission) !=
+                PackageManager.PERMISSION_GRANTED
+            ) {
+                return false
+            }
+        }
+        return true
     }
 
     override fun onRequestPermissionsResult(
@@ -53,15 +67,21 @@ class JiaoZiVideoPlayerActivity : BaseActivity() {
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         when (requestCode) {
-            1 ->
-                if (grantResults.isNotEmpty() &&
-                    grantResults[0] == PackageManager.PERMISSION_GRANTED
-                ) {
+            1 -> {
+                var allPermissionsGranted = true
+                for (result in grantResults) {
+                    if (result != PackageManager.PERMISSION_GRANTED) {
+                        allPermissionsGranted = false
+                        break
+                    }
+                }
+                if (allPermissionsGranted) {
                     // 应用外响应
                     videoplayer.setUp(data.toString(), data.toString(), JzvdStd.SCREEN_NORMAL)
                 } else {
                     Toast.makeText(this, "你拒绝了权限", Toast.LENGTH_SHORT).show()
                 }
+            }
         }
     }
 
