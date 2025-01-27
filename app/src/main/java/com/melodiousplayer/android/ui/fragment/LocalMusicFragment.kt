57865f2124d6c7093ce342af57c1898da8091bad
LMINediva <3037907347@qq.com>
2025-01-27 22:17:06 +0800

    Add dynamic permission request code.

--- app/src/main/java/com/melodiousplayer/android/ui/fragment/LocalMusicFragment.kt
+++ app/src/main/java/com/melodiousplayer/android/ui/fragment/LocalMusicFragment.kt
@@ -1,10 +1,14 @@
 package com.melodiousplayer.android.ui.fragment
 
+import android.Manifest
 import android.content.AsyncQueryHandler
+import android.content.pm.PackageManager
 import android.database.Cursor
 import android.provider.MediaStore.Audio.Media
 import android.view.View
 import android.widget.ListView
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import com.melodiousplayer.android.R
 import com.melodiousplayer.android.adapter.LocalMusicAdapter
 import com.melodiousplayer.android.base.BaseFragment
@@ -28,6 +32,79 @@ class LocalMusicFragment : BaseFragment() {
     override fun initData() {
         listView = view.findViewById(R.id.listView)
         adapter = LocalMusicAdapter(context, null)
+        // 动态权限申请
+        handlePermission()
+    }
+
+    /**
+     * 处理权限问题
+     */
+    private fun handlePermission() {
+        val permission = Manifest.permission.READ_EXTERNAL_STORAGE
+        // 查看是否有权限
+        val checkSelfPermission = context?.let {
+            ActivityCompat.checkSelfPermission(it, permission)
+        }
+        if (checkSelfPermission == PackageManager.PERMISSION_GRANTED) {
+            // 已经获取权限
+            loadSongs()
+        } else {
+            // 没有获取权限
+            if (ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    permission
+                )
+            ) {
+                // 需要弹出
+                AlertDialog.Builder(requireContext()).apply {
+                    setTitle("温馨提示")
+                    setMessage("我们只会访问音乐文件，不会访问隐私照片")
+                    setCancelable(false)
+                    setPositiveButton("OK") { dialog, which ->
+                        myRequestPermission()
+                    }
+                    setNegativeButton("Cancel") { dialog, which ->
+
+                    }
+                    show()
+                }
+            } else {
+                // 不需要弹出
+                myRequestPermission()
+            }
+        }
+    }
+
+    /**
+     * 真正申请权限
+     */
+    private fun myRequestPermission() {
+        val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+        ActivityCompat.requestPermissions(requireActivity(), permissions, 1)
+    }
+
+    /**
+     * 接收权限授权结果
+     * requestCode 请求码
+     * permissions 权限申请数组
+     * grantResults 申请之后的结果
+     */
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    loadSongs()
+                }
+            }
+        }
+    }
+
+    private fun loadSongs() {
         // 加载音乐列表数据
         val resolver = context?.contentResolver
         val handler = object : AsyncQueryHandler(resolver) {
