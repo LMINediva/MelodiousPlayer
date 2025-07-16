20235c3776e5cbbe556ed9780c1aa69d251b3e37
LMINediva <3037907347@qq.com>
2025-07-16 13:18:06 +0800

    Improve the data list on the homepage.

--- app/src/main/java/com/melodiousplayer/android/model/HomeItemBean.kt
+++ app/src/main/java/com/melodiousplayer/android/model/HomeItemBean.kt
@@ -7,7 +7,7 @@ data class HomeItemBean(
     var type: String, var id: Int,
     var title: String, var description: String?,
     var posterPic: String?, var thumbnailPic: String?,
-    var url: String, var hdUrl: String?, var videoSize: Int,
-    var hdVideoSize: Int, var uhdVideoSize: Int, var status: Int,
+    var url: String, var hdUrl: String?, var musicSize: Int,
+    var hdMusicSize: Int, var uhdMusicSize: Int, var status: Int,
     var traceUrl: String?, var clickUrl: String?, var uhdUrl: String?
 )
