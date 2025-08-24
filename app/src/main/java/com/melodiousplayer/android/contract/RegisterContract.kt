e4ac60f64a7e911c79fbd13c4a81642399a64b6b
LMINediva <3037907347@qq.com>
2025-08-24 11:19:30 +0800

    Implement the MVP protocol for the registration interface.

---
+++ app/src/main/java/com/melodiousplayer/android/contract/RegisterContract.kt
@@ -0,0 +1,21 @@
+package com.melodiousplayer.android.contract
+
+/**
+ * 注册协议持久化接口
+ */
+interface RegisterContract {
+
+    interface Presenter : BasePresenter {
+        fun register(userName: String, password: String, confirmPassword: String)
+    }
+
+    interface View {
+        fun onUserNameError()
+        fun onPasswordError()
+        fun onConfirmPasswordError()
+        fun onStartRegister()
+        fun onRegisterSuccess()
+        fun onRegisterFailed()
+    }
+
+}
