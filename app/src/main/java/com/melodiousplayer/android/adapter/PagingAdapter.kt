9253e8c5c37d632f2ad6fadce33e05621a20d748
LMINediva <3037907347@qq.com>
2025-10-07 14:31:26 +0800

    Complete the MV list interface and its adapter.

--- app/src/main/java/com/melodiousplayer/android/adapter/PagingAdapter.kt
+++ app/src/main/java/com/melodiousplayer/android/adapter/PagingAdapter.kt
@@ -3,17 +3,28 @@ package com.melodiousplayer.android.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
+import android.widget.CheckBox
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.melodiousplayer.android.R
 import com.melodiousplayer.android.model.VideosBean
+import com.melodiousplayer.android.util.URLProviderUtils
 
+/**
+ * MV列表分页表格适配器
+ */
 class PagingAdapter(
+    private var items: List<VideosBean>,
+    private val selectedItems: Set<VideosBean>,
     private val onItemSelected: (VideosBean, Boolean) -> Unit
-) : ListAdapter<VideosBean, PagingAdapter.ViewHolder>(DiffCallback()) {
+) : RecyclerView.Adapter<PagingAdapter.ViewHolder>() {
 
-    inner class ViewHolder(val itemView: View) : RecyclerView.ViewHolder(itemView)
+    fun updateItems(newItems: List<VideosBean>) {
+        items = newItems
+        notifyDataSetChanged()
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -21,21 +32,36 @@ class PagingAdapter(
         return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+    override fun getItemCount(): Int {
+        return items.size
     }
 
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = items[position]
+        holder.bind(item, selectedItems.contains(item), onItemSelected)
+    }
 
-    class DiffCallback : DiffUtil.ItemCallback<VideosBean>() {
-
-        override fun areItemsTheSame(oldItem: VideosBean, newItem: VideosBean): Boolean {
-            return oldItem.id == newItem.id
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(
+            item: VideosBean,
+            isSelected: Boolean,
+            onItemSelected: (VideosBean, Boolean) -> Unit
+        ) {
+            val title: TextView = itemView.findViewById(R.id.title)
+            val artistName: TextView = itemView.findViewById(R.id.artistName)
+            val posterPicture: ImageView = itemView.findViewById(R.id.posterPicture)
+            val checkBox: CheckBox = itemView.findViewById(R.id.checkBox)
+            title.text = item.title
+            artistName.text = item.artistName
+            Glide.with(itemView.context).load(
+                URLProviderUtils.protocol + URLProviderUtils.serverAddress
+                        + URLProviderUtils.mvImagePath + item.posterPic
+            ).into(posterPicture)
+            checkBox.isChecked = isSelected
+            checkBox.setOnCheckedChangeListener { _, isChecked ->
+                onItemSelected(item, isChecked)
+            }
         }
-
-        override fun areContentsTheSame(oldItem: VideosBean, newItem: VideosBean): Boolean {
-            return oldItem == newItem
-        }
-
     }
 
 }
--- app/src/main/java/com/melodiousplayer/android/ui/activity/AddMusicListActivity.kt
+++ app/src/main/java/com/melodiousplayer/android/ui/activity/AddMusicListActivity.kt
@@ -1,15 +1,34 @@
 package com.melodiousplayer.android.ui.activity
 
+import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.melodiousplayer.android.R
+import com.melodiousplayer.android.adapter.PagingAdapter
 import com.melodiousplayer.android.base.BaseActivity
+import com.melodiousplayer.android.model.VideosBean
 import com.melodiousplayer.android.util.ToolBarManager
 
 /**
  * 添加音乐清单界面
  */
 class AddMusicListActivity : BaseActivity(), ToolBarManager {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: PagingAdapter
+    private lateinit var btnPrevious: Button
+    private lateinit var btnNext: Button
+    private lateinit var pageInfo: TextView
+    private lateinit var total: TextView
+    private lateinit var spinnerPageSize: Spinner
+    private var currentPage = 1
+    private var totalPages = 1
+    private var pageSize = 0
+    private val dataList = mutableListOf<VideosBean>()
+    private val selectedItems = mutableSetOf<VideosBean>()
 
     override val toolbar by lazy { findViewById<Toolbar>(R.id.toolbar) }
     override val toolbarTitle by lazy { findViewById<TextView>(R.id.toolbar_title) }
@@ -29,6 +48,29 @@ class AddMusicListActivity : BaseActivity(), ToolBarManager {
             // 隐藏默认标题
             it.setDisplayShowTitleEnabled(false)
         }
+        recyclerView = findViewById(R.id.recyclerView)
+        btnPrevious = findViewById(R.id.btnPrevious)
+        btnNext = findViewById(R.id.btnNext)
+        pageInfo = findViewById(R.id.pageInfo)
+        total = findViewById(R.id.total)
+        spinnerPageSize = findViewById(R.id.spinnerPageSize)
+        // 设置RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = PagingAdapter(dataList, selectedItems) {item, isChecked ->
+            if (isChecked) {
+                selectedItems.add(item)
+            } else {
+                selectedItems.remove(item)
+            }
+        }
+        recyclerView.adapter = adapter
+    }
+
+    /**
+     * 初始化MV列表
+     */
+    private fun initMVList() {
+        
     }
 
 }
