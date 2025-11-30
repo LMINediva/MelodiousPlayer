6d3cdb9dad0044e60f06c9b343a3c02b6e75cea9
LMINediva <3037907347@qq.com>
2025-11-30 14:05:53 +0800

    Optimize the video description page.

--- app/src/main/java/com/melodiousplayer/android/ui/fragment/VideoDescriptionFragment.kt
+++ app/src/main/java/com/melodiousplayer/android/ui/fragment/VideoDescriptionFragment.kt
@@ -1,8 +1,8 @@
 package com.melodiousplayer.android.ui.fragment
 
-import android.graphics.Color
 import android.view.View
 import android.widget.TextView
+import com.melodiousplayer.android.R
 import com.melodiousplayer.android.base.BaseFragment
 
 /**
@@ -10,19 +10,31 @@ import com.melodiousplayer.android.base.BaseFragment
  */
 class VideoDescriptionFragment : BaseFragment() {
 
+    private lateinit var view: View
+    private lateinit var descriptionTextView: TextView
     private var description: String? = null
 
     override fun initView(): View? {
+        if (!::view.isInitialized) {
+            view = View.inflate(context, R.layout.fragment_video_description, null)
+        }
+        return view
+    }
+
+    override fun initData() {
+        descriptionTextView = view.findViewById(R.id.description)
         // 获取视频描述数据
         description = arguments?.getString("description")
-        val tv = TextView(context)
-        tv.setTextColor(Color.BLACK)
         if (description != null) {
-            tv.text = "\u3000\u3000" + description
+            descriptionTextView.text = addFirstLineIndent(description!!, 4)
         } else {
-            tv.text = "\u3000\u3000" + "暂无视频描述"
+            descriptionTextView.text = addFirstLineIndent("暂无视频描述", 4)
         }
-        return tv
+    }
+
+    private fun addFirstLineIndent(text: String, indent: Int): String {
+        val indentSpaces = String(CharArray(indent)).replace('\u0000', '\t')
+        return indentSpaces + text
     }
 
 }
