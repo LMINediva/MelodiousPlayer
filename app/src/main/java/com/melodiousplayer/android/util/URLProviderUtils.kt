66c72233c3ae2ac33d210d11f3a0edd961f14678
LMINediva <3037907347@qq.com>
2025-12-24 14:09:40 +0800

    Optimize the UI of the login button.

--- app/src/main/java/com/melodiousplayer/android/util/URLProviderUtils.kt
+++ app/src/main/java/com/melodiousplayer/android/util/URLProviderUtils.kt
@@ -9,7 +9,7 @@ import android.util.Log
 object URLProviderUtils {
 
     const val protocol = "http://"
-    var serverAddress = "tx.frpee.com:17116"
+    var serverAddress = "hk.frpee.top:17116"
     const val musicImagePath = "/image/musicPicture/"
     const val musicPath = "/audio/music/"
     const val lyricPath = "/audio/lyric/"
